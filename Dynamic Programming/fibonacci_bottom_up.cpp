5a94bc9169036e96513c83c36c586ade7c2b92c3
shubhamkr01 <skshubham2014@gmail.com>
2017-06-23 13:45:07 +0530

    Update fibonacci_bottom_up.cpp

--- Dynamic Programming/fibonacci_bottom_up.cpp
+++ Dynamic Programming/fibonacci_bottom_up.cpp
@@ -1,1 +1,20 @@
+//CPP program to find the n-th fibonacci no. using dynamic programming (bottom-up approach)
 
+#include <iostream>
+using namespace std;
+
+int fib(int n){   // function to find the n-th fibonacci
+    int f[n];
+    f[0]=1;
+    f[1]=1;
+    for(int i=2; i<=n; i++)
+        f[i] = f[i-1] + f[i-2];
+    return f[n-1];
+}
+
+int main() {    // main/driver function 
+	int n;
+	cin>>n;
+	cout<<fib(n);
+	return 0;
+}
